5f508642e347b4a2a17bc12905c43e3eeb14ad50
Daniel <Daniel@192.168.1.10>
2016-05-22 16:29:47 +0200

    Examen DaNy123123

---
+++ src/pruebaexamen/DaNy123123/ExamenDaNy123123.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaexamen.DaNy123123;
+
+/**
+ *
+ * @author Daniel
+ */
+public class ExamenDaNy123123 {
+    
+    public ExamenDaNy123123 ()
+    {
+        System.out.print("Hola me llamo DaNy123123");
+    
+    }
+    
+}
